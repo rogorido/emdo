daef532696d00d10a4725e4060d4a73cd707c3ea
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-11-25 21:39:22 +0100

    Getdatapage.

---
+++ src/pages/GetDataPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-page>
+    <h2>Early Modern Order's Preacher</h2>
+
+    <p>
+      El proyecto
+      <em>
+        Religious globalisation as challenge. Global governance, geopolitics and
+        costs in the Order of Preachers (1570-1700)
+      </em>
+      se vincula a la idea de la
+      <a href="https://digital.csic.es/handle/10261/145975" target="_blank">
+        ciencia reproducible</a
+      >, por lo que tantos los datos como los pasos para llegar a los análisis,
+      o en otras palabras, el código usado (javascript, SQL, python, etc.) está
+      disponible para otros investigadores.
+    </p>
+    <h2>Accesibilidad de los datos</h2>
+    <p>Más concretamente:</p>
+    <ul>
+      <li>
+        los datos brutos en forma de fichero dump de la base de datos PostgrSQL
+        usada se puede encontrar
+        <a href="https://www.github.com/rogorido/dominicansdb">
+          en la página de github</a
+        >
+      </li>
+      <li>
+        los datos sobre las casas dominicas se encuentran en la base de datos
+        mencionada y se puede instalar una API en un servidor propio con el
+        código que se encuentra en
+        <a href="https://github.com/rogorido/apihouses">
+          en la página de github</a
+        >
+      </li>
+    </ul>
+
+    <h2>Licencias en uso</h2>
+    <p>
+      Es importante que aquellos interesados en los datos brutos y la
+      infraestructura informática de esta página sean conscientes de las
+      licencias bajo las que están liberados tantos los datos como el código
+      informático. Excepto cuando se diga lo contrario, usaremos estas
+      licencias:
+    </p>
+
+    <ul>
+      <li>
+        El código (por ejemplo javascript, etc.) está liberado bajo la licencia
+        <a href="https://www.gnu.org/licenses/gpl-3.0.html">
+          GPL license version 3</a
+        >
+        Se halla disponible en mi
+        <a href="https://www.github.com/rogorido" target="_blank">
+          cuenta de github
+        </a>
+        .
+      </li>
+      <li>
+        Las bases de datos se encuentra liberadas bajo la licencia
+        <a href="https://opendatacommons.org/licenses/odbl/" target="_blank">
+          Open Data Commons Open Database License (ODbL, v.1.0)</a
+        >
+        . O, en otras palabras, los interesados pueden compartir, usar y adaptar
+        los datos siempre y cuando citen esta página y mi nombre y permitan a
+        otros hacer lo mismo.
+      </li>
+    </ul>
+  </q-page>
+</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,21 @@
 const routes = [
   {
-    path: "/",
-    component: () => import("layouts/MainLayout.vue"),
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "/bishops", component: () => import("pages/BishopsPage.vue") },
-      { path: "/about", component: () => import("pages/AboutPage.vue") },
-      { path: "/contact", component: () => import("pages/ContactPage.vue") },
-    ],
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/bishops', component: () => import('pages/BishopsPage.vue') },
+      { path: '/about', component: () => import('pages/AboutPage.vue') },
+      { path: '/contact', component: () => import('pages/ContactPage.vue') },
+      { path: '/getdata', component: () => import('pages/GetDataPage.vue') }
+    ]
   },
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
-  },
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
 ];
 
 export default routes;
