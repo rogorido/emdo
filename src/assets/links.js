86b7c38927ff766d6daf5284056dd6e8b81417e5
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-11-25 21:40:12 +0100

    Formatting json.

--- src/assets/links.js
+++ src/assets/links.js
@@ -1,56 +1,82 @@
 export const linksList = [
   {
-    title: "Home",
-    icon: "school",
-    link: "/",
+    title: 'Home',
+    icon: 'school',
+    link: '/'
   },
 
   {
-    title: "Bishops",
-    caption: "Bishops",
-    icon: "school",
-    link: "/bishops",
+    title: 'Bishops',
+    caption: 'Bishops',
+    icon: 'school',
+    link: '/bishops'
   },
   {
-    title: "Chapters",
-    caption: "General chapters",
-    icon: "keyboard_command_key",
-    link: "/chapters",
+    title: 'Chapters',
+    caption: 'General chapters',
+    icon: 'keyboard_command_key',
+    link: '/chapters'
   },
   {
-    title: "Resolutions",
-    caption: "Resolutions of the General Chapters",
-    icon: "code",
-    link: "/resolutions",
+    title: 'Resolutions',
+    caption: 'Resolutions of the General Chapters',
+    icon: 'code',
+    link: '/resolutions'
   },
   {
-    title: "Houses",
-    caption: "Infrastructure of the Order",
-    icon: "chat",
-    link: "/houses",
+    title: 'Houses',
+    caption: 'Infrastructure of the Order',
+    icon: 'chat',
+    link: '/houses'
   },
   {
-    title: "Contact",
-    caption: "Infrastructure of the Order",
-    icon: "chat",
-    link: "/contact",
+    title: 'Contact',
+    caption: 'Infrastructure of the Order',
+    icon: 'chat',
+    link: '/contact'
   },
+  {
+    title: 'Get the data',
+    caption: 'Infrastructure of the Order',
+    icon: 'chat',
+    link: '/getdata'
+  }
 ];
 
 export const sections = [
-  { id: 1, titulo: "Houses", texto: "leches", link: "/houses" },
-  { id: 2, titulo: "Bishops", texto: "hola", link: "/bishops" },
+  {
+    id: 1,
+    titulo: 'Houses',
+    texto: 'leches',
+    link: '/houses',
+    image: 'holyland-circle.png'
+  },
+  {
+    id: 2,
+    titulo: 'Bishops',
+    texto: 'hola',
+    link: '/bishops',
+    image: 'bishops.png'
+  },
   {
     id: 3,
-    titulo: "General Chapters",
-    texto: "ahora va más",
-    link: "/chapters",
+    titulo: 'General Chapters',
+    texto: 'ahora va más',
+    link: '/chapters',
+    image: 'cagayan.png'
   },
   {
     id: 4,
-    titulo: "Chapters' resolutions",
-    texto: "ahora va más",
-    link: "/resolutions",
+    titulo: 'Resolutions',
+    texto: 'ahora va más',
+    link: '/resolutions',
+    image: 'islas-marianas.png'
   },
-  { id: 5, titulo: "Works", texto: "ahora va más", link: "/works" },
+  {
+    id: 5,
+    titulo: 'Works',
+    texto: 'ahora va más',
+    link: '/works',
+    image: 'chronicon-icon.png'
+  }
 ];
