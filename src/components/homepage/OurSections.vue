a6a19ac17ea76372d36d99deca3608e9f62bcd7d
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-11-29 23:27:17 +0100

    Sections written fully.

--- src/components/homepage/OurSections.vue
+++ src/components/homepage/OurSections.vue
@@ -1,32 +1,126 @@
 <template>
-  <section class="probando">
+  <section>
     <h4 class="text-center">Our sections</h4>
 
     <div
-      class="row q-mb-lg oferta q-hoverable cursor-pointer"
-      v-for="section in sections"
-      :key="section.id"
-      @click="goToSite(section.link)"
+      class="row q-mb-xl oferta q-hoverable cursor-pointer q-col-gutter-lg"
+      @click="goToSite('/houses')"
     >
       <div class="col imageshown">
         <q-img
-          :src="`images/${section.image}`"
-          :alt="section.title"
-          height="250px"
+          src="images/archipel-sanlazaro-colored.webp"
+          alt="houses"
+          height="400px"
+          style="border-radius: 10% 30% 50% 70%"
         ></q-img>
       </div>
       <div class="col descriptionshown">
-        <h5 class="q-mt-none q-mb-sm text-center text-uppercase">
-          {{ section.titulo }}
+        <h5 class="q-mt-none q-mb-sm text-center text-uppercase text-primary">
+          Houses
         </h5>
-        <p>{{ section.texto }}</p>
+        <p>
+          Dominican houses are important feature of the Order. The question is
+          how many houses were present in the world.
+        </p>
+      </div>
+    </div>
+
+    <div
+      class="row q-mb-xl oferta q-hoverable cursor-pointer q-col-gutter-x-lg"
+      @click="goToSite('/bishops')"
+    >
+      <div class="col imageshown">
+        <q-img
+          src="images/bishops.webp"
+          alt="bishops"
+          height="400px"
+          style="border-radius: 10% 30% 50% 70%"
+        ></q-img>
+      </div>
+      <div class="col descriptionshown">
+        <h5 class="q-mt-none q-mb-sm text-center text-uppercase text-primary">
+          Bishops
+        </h5>
+        <p>
+          Dominican houses are important feature of the Order. The question is
+          how many houses were present in the world.
+        </p>
+      </div>
+    </div>
+
+    <div
+      class="row q-mb-xl oferta q-hoverable cursor-pointer q-col-gutter-x-lg"
+      @click="goToSite('/chapters')"
+    >
+      <div class="col imageshown">
+        <q-img
+          src="images/cagayan.png"
+          alt="chapters"
+          height="400px"
+          style="border-radius: 10% 30% 50% 70%"
+        ></q-img>
+      </div>
+      <div class="col descriptionshown">
+        <h5 class="q-mt-none q-mb-sm text-center text-uppercase text-primary">
+          Chapters
+        </h5>
+        <p>
+          Dominican houses are important feature of the Order. The question is
+          how many houses were present in the world.
+        </p>
+      </div>
+    </div>
+
+    <div
+      class="row q-mb-xl oferta q-hoverable cursor-pointer q-col-gutter-x-lg"
+      @click="goToSite('/resolutions')"
+    >
+      <div class="col imageshown">
+        <q-img
+          src="images/cagayan.png"
+          alt="resolutions"
+          height="400px"
+          style="border-radius: 10% 30% 50% 70%"
+        ></q-img>
+      </div>
+      <div class="col descriptionshown">
+        <h5 class="q-mt-none q-mb-sm text-center text-uppercase text-primary">
+          Resolutions
+        </h5>
+        <p>
+          Dominican houses are important feature of the Order. The question is
+          how many houses were present in the world.
+        </p>
+      </div>
+    </div>
+
+    <div
+      class="row q-mb-xl oferta q-hoverable cursor-pointer q-col-gutter-x-lg"
+      @click="goToSite('/worlks')"
+    >
+      <div class="col imageshown">
+        <q-img
+          src="images/chronicon-icon.png"
+          alt="works"
+          height="400px"
+          fit="contain"
+          style="border-radius: 10% 30% 50% 70%"
+        ></q-img>
+      </div>
+      <div class="col descriptionshown">
+        <h5 class="q-mt-none q-mb-sm text-center text-uppercase text-primary">
+          Works
+        </h5>
+        <p>
+          Dominican houses are important feature of the Order. The question is
+          how many houses were present in the world.
+        </p>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-import { sections } from '../../assets/links';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
