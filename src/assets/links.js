47a61c9ddcd811e7f036fcd27f2f322930ea7e08
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-11-30 12:23:39 +0100

    Reducing menu.

--- src/assets/links.js
+++ src/assets/links.js
@@ -1,34 +1,28 @@
 export const linksList = [
   {
+    title: 'Home',
+    icon: 'school',
+    link: '/'
+  },
+
+  {
+    title: 'Our sections',
+    caption: 'What you find here',
+    icon: 'school',
+    link: '/oursections'
+  },
+  {
+    title: 'Links',
+    icon: 'school',
+    link: '/links'
+  },
+
+  {
     title: 'About',
     icon: 'school',
     link: '/about'
   },
 
-  {
-    title: 'Bishops',
-    caption: 'Bishops',
-    icon: 'school',
-    link: '/bishops'
-  },
-  {
-    title: 'Chapters',
-    caption: 'General chapters',
-    icon: 'keyboard_command_key',
-    link: '/chapters'
-  },
-  {
-    title: 'Resolutions',
-    caption: 'Resolutions of the General Chapters',
-    icon: 'code',
-    link: '/resolutions'
-  },
-  {
-    title: 'Houses',
-    caption: 'Infrastructure of the Order',
-    icon: 'chat',
-    link: '/houses'
-  },
   {
     title: 'Contact',
     caption: 'Infrastructure of the Order',
