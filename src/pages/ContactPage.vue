0f03ea059419957792e2342f5a84b4ca77a6abf3
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-12-02 20:03:18 +0100

    Small aesthetics change.

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -41,4 +41,14 @@
     información pertinente en
     <router-link class="textlinks" to="/getdata">esta página</router-link>.
   </p>
+  <div class="flex flex-center"><div class="separador"></div></div>
 </template>
+
+<style lang="scss" scoped>
+.separador {
+  margin: 30px 0;
+  background-image: url('../assets/svg/pattern_05.svg');
+  height: 200px;
+  width: 100%;
+}
+</style>
