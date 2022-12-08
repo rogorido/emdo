981b3250ba034d4ec6bb035e3e17528d57f510fe
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2022-12-08 19:07:17 +0100

    New technicaldatapage.

---
+++ src/pages/TechnicalDataPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3 class="text-center">Technical data</h3>
+
+  <div class="flex flex-center"><div class="separador"></div></div>
+</template>
+
+<style lang="scss" scoped>
+.separador {
+  margin: 30px 0;
+  background-image: url('../assets/svg/pattern_05.svg');
+  height: 200px;
+  width: 100%;
+}
+</style>
