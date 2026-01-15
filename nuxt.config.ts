f0c23f948916771a1651e5772c478eef217f55af
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2026-01-15 20:49:04 +0100

    Using modules for sitemap, robots, etc.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,8 @@ export default defineNuxtConfig({
   modules: [
     '@primevue/nuxt-module',
     '@nuxt/fonts',
-    '@nuxtjs/sitemap'
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots'
   ],
 
   primevue: { options: { theme: { preset: Aura } } },
