09be5eecaa9257cdd85b0ce4fda13524e16ac8a7
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2026-01-15 20:07:31 +0100

    Primevue, primeicons and other important packages.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
+const isProdEnv = process.env.NODE_ENV === 'production';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ['@primevue/nuxt-module'],
+
+  primevue: { options: { theme: { preset: Aura } } },
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: isProdEnv
+        ? process.env.API_BASE_URL
+        : 'http://localhost:8001',
+      uiVersion: process.env.npm_package_version
+    }
+  }
+});
